1f8e46a60040cc79fad5a4be2ec7d6c6c346f3f2
avazagit <j.murray@avaza.co>
2014-09-11 17:36:41 -0500

    start development
    first commit

---
+++ app/views/pages/home.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.default')
+
+@section('content')
+    <div class="jumbotron">
+        <h1>Welcome to the ARC System!</h1>
+            <p>You're gonna love this....</p>
+        <p><a class="btn btn-primary btn-lg" role="button">Get Started</a></p>
+    </div>
+
+@stop
